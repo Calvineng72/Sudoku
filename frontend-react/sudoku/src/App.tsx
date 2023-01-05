d46dae7a1ef6774883db4fbec1d65f7dfbea6b83
Calvineng72 <calvineng72@gmail.com>
2023-01-05 12:15:21 -0700

    feat: switching to react from next.js

---
+++ frontend-react/sudoku/src/App.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import './App.css';
+import createGameEnvironment from './SudokuBoard';
+import Header from './Header';
+
+export default function App() {  
+  return (
+    <div className='website'>
+      <Header></Header>
+      <div id='puzzle' className='puzzle'>
+        {createGameEnvironment().map((square) => square)}
+      </div>
+      <button onClick={createPuzzle}>Create Puzzle</button>
+      <button onClick={check}>Check</button>
+      <button onClick={solve}>Solve</button>
+      <button onClick={clear}>Clear</button>
+    </div>
+  )
+}
+
+let solution: number[][] = [];
+let puzzle: number[][] = [];
+
+async function fetchPuzzle(): Promise<void> {
+  await fetch('http://localhost:3232/getSudoku')
+    .then((r) => r.json())
+    .then((json) => {
+      solution = json.solution;
+      puzzle = json.sudoku;
+    });
+}
+
+async function createPuzzle() {
+  clear();
+  await fetchPuzzle();
+  const inputs = document.querySelectorAll('input');
+  puzzle.forEach((row, rowIndex) => {
+    row.forEach((value, colIndex) => {
+      const index = rowIndex * 9 + colIndex;
+      const input = inputs[index];
+        if (value !== 0) {
+          input.value = value.toString();
+          input.setAttribute('disabled', 'true')
+        }
+    });
+  });
+}
+
+function check() {
+  const inputs = document.querySelectorAll('input');
+  solution.forEach((row, rowIndex) => {
+    row.forEach((value, colIndex) => {
+      const index = rowIndex * 9 + colIndex;
+      const input = inputs[index];
+        if (parseInt(input.value) === value) {
+          input.style.color = 'green';
+          input.setAttribute('disabled', 'true');
+        }
+        else if (input.value !== '') {
+          input.style.color = 'red';
+        }
+    });
+  });
+}
+
+function solve() {
+  const inputs = document.querySelectorAll('input');
+  solution.forEach((row, rowIndex) => {
+    row.forEach((value, colIndex) => {
+      const index = rowIndex * 9 + colIndex;
+      const input = inputs[index];
+      input.value = value.toString();
+      input.style.color = 'black';
+      input.setAttribute('disabled', 'true');
+    });
+  });
+}
+
+function clear() {
+  const inputs = document.querySelectorAll('input');
+  inputs.forEach((input) => {
+    input.value = ''
+    input.style.color = 'black'
+    input.removeAttribute('disabled');
+  });
+}
---
+++ frontend-react/sudoku/src/SudokuBoard.tsx
@@ -0,0 +1,24 @@
+import './SudokuBoard.css'
+
+export default function createGameEnvironment(): JSX.Element[] {
+    let className: string = '';
+    let html: JSX.Element[] = [];
+  
+    for (let i = 0; i < 81; i++) {
+        if (((i % 9 === 0 || i % 9 === 1 || i % 9 === 2) && i < 21) ||
+            ((i % 9 === 6 || i % 9 === 7 || i % 9 === 8) && i < 27) ||
+            ((i % 9 === 3 || i % 9 === 4 || i % 9 === 5) && (i > 27 && i < 53)) ||
+            ((i % 9 === 0 || i % 9 === 1 || i % 9 === 2) && i > 53) ||
+            ((i % 9 === 6 || i % 9 === 7 || i % 9 === 8) && i > 53)) {
+            className = 'coloredSquare';
+        }
+        else {
+            className = 'square';
+        }
+
+        html.push(<input className={className} key={i} type="text" pattern="[0-9]"
+            inputMode='numeric' maxLength={1}/>);
+    }
+    
+    return html;
+}  
